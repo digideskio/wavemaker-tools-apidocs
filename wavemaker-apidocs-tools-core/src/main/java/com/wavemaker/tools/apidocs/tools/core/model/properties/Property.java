d1ce42fba6664b57649361badde42e4f3d814435
Dilip Kumar <dilip.gundu@wavemaker.com>
2015-03-24 22:20:12 +0530

    0# Added deserializer for Property
    Change-Id: Ia1b4aa7a988670d9ef256d19b83ddfa44e858753

--- wavemaker-apidocs-tools-core/src/main/java/com/wavemaker/tools/apidocs/tools/core/model/properties/Property.java
+++ wavemaker-apidocs-tools-core/src/main/java/com/wavemaker/tools/apidocs/tools/core/model/properties/Property.java
@@ -1,8 +1,11 @@
 package com.wavemaker.tools.apidocs.tools.core.model.properties;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.wavemaker.tools.apidocs.tools.core.deserializers.PropertyDeserializer;
 import com.wavemaker.tools.apidocs.tools.core.model.Xml;
 
+@JsonDeserialize(using = PropertyDeserializer.class)
 public interface Property {
     Property title(String title);
 
